7983b209e361a5f26d148ce7f9291f399bff717e
Andreas Hanft <>
2020-04-27 14:28:38 +0200

    Removed RxSwift dependency

--- Package.swift
+++ Package.swift
@@ -10,33 +10,13 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "XCoordinator",
-            targets: ["XCoordinator"]),
-        .library(
-            name: "XCoordinatorRx",
-            targets: ["XCoordinatorRx"]),
-        .library(
-            name: "XCoordinatorCombine",
-            targets: ["XCoordinatorCombine"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.1"),
+            targets: ["XCoordinator"])
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XCoordinator",
-            dependencies: []),
-        .target(
-            name: "XCoordinatorRx",
-            dependencies: ["XCoordinator", "RxSwift"]),
-        .target(
-            name: "XCoordinatorCombine",
-            dependencies: ["XCoordinator"]),
-        .testTarget(
-            name: "XCoordinatorTests",
-            dependencies: ["XCoordinator", "XCoordinatorRx"]),
+            dependencies: [])
     ]
 )
